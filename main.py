f7dc9a691f4efcb74183df99e23bae893811e95f
Abdelrahman Ahmed <ahm_mshmsh2004@yahoo.com>
2026-01-23 22:21:09 +0200

    include base router in main

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # import libraries
 from fastapi import FastAPI
+from routes import base
 
 # fastAPI app
 app = FastAPI()
+
+# include the base router created in base
+app.include_router(base.base_router)
