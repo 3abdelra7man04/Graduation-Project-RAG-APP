4bba71cef96111e49d73f24328df1aafd0740975
Abdelrahman Ahmed <ahm_mshmsh2004@yahoo.com>
2026-02-05 16:23:17 +0200

    initialize db connection in main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
 # import libraries
 from fastapi import FastAPI
 from routes import base, data
+from motor.motor_asyncio import AsyncioMotorClient
+from helpers.config import get_settings
 
 # fastAPI app
 app = FastAPI()
+
+# mongo connection startup
+@app.on_event("startup")
+async def startup_db_client():
+
+    settings = get_settings() # get app settings
+
+    app.mongo_connection = AsyncioMotorClient(settings.MONGODB_URL) # initialize mongo connection
+
+    app.db_client = app.mongo_connection[settings.MONGODB_DATABASE] # create db client
+
+# mongo connection shutdown
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongo_connection.close()
+
 
 # include the base router created in base.py
 app.include_router(base.base_router)
