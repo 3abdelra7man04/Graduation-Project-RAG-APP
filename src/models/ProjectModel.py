1f7c6d46e82f411ba7e1c477e5b4d16f80fd2488
Abdelrahman Ahmed <ahm_mshmsh2004@yahoo.com>
2026-02-08 13:44:42 +0200

    make sure that dict sees the alias and exclude the unset fields

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -9,7 +9,7 @@ class ProjectModel(BaseDataModel):
     
     # add a new project to collection
     async def add_project(self, project: Project):
-        result = await self.collection.insert_one(project.dict())
+        result = await self.collection.insert_one(project.dict(by_alias = True, exclude_unset = True))
         project.id = result.inserted_id
 
         return project
