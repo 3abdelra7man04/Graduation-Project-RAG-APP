52319db20fe5633b2c9957d96c26faba44e62920
Abdelrahman Ahmed <ahm_mshmsh2004@yahoo.com>
2026-03-22 16:53:48 +0200

    created function to get a db path

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -16,6 +16,18 @@ class BaseController:
         # Path to stored file assets
         self.files_dir = os.path.join(self.base_dir, "assets/files")
 
+        # Path to stored database assets
+        self.database_dir = os.path.join(self.base_dir, "assets/database")
+
     def generate_random_string(self, length: int = 12):
         # Generate a random alphanumeric string
         return "".join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    
+    # get database directory
+    def get_database_path(self, db_path: str):
+        database_path = os.path.join(self.database_dir, db_path)
+
+        if not os.path.exists(database_path):
+            os.makedirs(database_path)
+        
+        return database_path
