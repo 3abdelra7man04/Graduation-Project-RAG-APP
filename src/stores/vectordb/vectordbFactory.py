e599b754a97037a054514b389a67dd13dcda913a
Abdelrahman Ahmed <ahm_mshmsh2004@yahoo.com>
2026-05-01 23:07:36 +0300

    changed qdrant client from local to use server client

--- src/stores/vectordb/vectordbFactory.py
+++ src/stores/vectordb/vectordbFactory.py
@@ -10,12 +10,7 @@ class VectordbFactory:
 
         # qdrant
         if provider == VectordbEnums.QDRANT.value:
-
-            # get database path using base controller
-            base_controller = BaseController()
-            db_path = base_controller.get_database_path(db_path=self.settings.VECTOR_DB_PATH)
-
-            return QdrantDBProvider(db_path= db_path, distance_method= self.settings.VECTOR_DB_DISTANCE_METHOD,)
+            return QdrantDBProvider(db_url= self.settings.VECTOR_DB_URL, distance_method= self.settings.VECTOR_DB_DISTANCE_METHOD,)
 
         return None
     
