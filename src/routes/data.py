ca53e374a310fc2dd55c99a323da8ff8251f6e06
Abdelrahman Ahmed <ahm_mshmsh2004@yahoo.com>
2026-05-12 00:59:39 +0300

    changed the upload route to include process and index in it and also created a list uploaded files route

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,18 +1,23 @@
-from fastapi import FastAPI, APIRouter, Depends, UploadFile, status, Request
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status, Request, BackgroundTasks
 from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController, ProcessController
+from controllers import DataController, ProjectController, ProcessController, NLPController
 import aiofiles
 from models import ResponseSignal
 import logging
-from .schemes.data import ProcessRequest
+from .schemes.data import ProcessRequest, UploadRequest
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
 from models.AssetModel import AssetModel
 from models.db_schemes.data_chunk import DataChunk
 from models.db_schemes.asset import Asset
 from models.enums.AssetTypeEnum import AssetTypeEnum
+from models.enums.AssetStatusEnum import AssetStatusEnum
+from models.enums.ProcesingEnums import ProcessingEnum
+import traceback
+from bson.objectid import ObjectId
+from datetime import datetime, timezone
 
 # Uvicorn logger instance
 logger = logging.getLogger("uvicorn.error")
@@ -23,6 +28,87 @@ data_router = APIRouter(
     tags=["api_v1", "data"],
 )
 
+# process and index background task
+async def process_and_index_pipeline(
+    request: Request,  # the reqeust object has the app and its data
+    project_id: str,
+    asset_id: str,
+    file_id: str,
+    db_client,
+    process_request: ProcessRequest,
+):
+
+    try:
+        project_model = await ProjectModel.create_instance(
+            db_client=db_client
+        )  # create the ProjectModel instance
+
+        project = await project_model.get_project_or_create_one(project_id=project_id)
+
+        chunk_size = process_request.chunk_size
+        chunk_overlap = process_request.chunk_overlap
+        do_reset = process_request.do_reset
+
+        asset_model = await AssetModel.create_instance(db_client=request.app.db_client)
+
+        # STEP 1: Update status to PROCESSING
+        _ = await asset_model.update_asset_status(asset_id, new_status= AssetStatusEnum.PROCESSING.value)
+
+        # create Process Controller object
+        process_cotroller = ProcessController(project_id=project_id)
+
+        chunk_model = await ChunkModel.create_instance(db_client=request.app.db_client)
+
+        if do_reset == True:
+            _ = await chunk_model.delete_chunks_by_project_id(project_id=project.id)
+
+        # get file content
+        file_content = process_cotroller.get_file_content(file_id)
+
+
+        # get file chunks
+        file_chunks = process_cotroller.process_file_content(
+            file_id=file_id,
+            file_content=file_content,
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+        )
+
+        file_chunks_records = [
+            DataChunk(
+                chunk_text=chunk.page_content,
+                chunk_metadata=chunk.metadata,
+                chunk_order=i + 1,
+                chunk_project_id=project.id,
+                chunk_asset_id=ObjectId(asset_id),
+            )
+            for i, chunk in enumerate(file_chunks)
+        ]
+        
+        _ =  await chunk_model.add_many_chunks(data_chunks=file_chunks_records)
+
+        # STEP 2: Save chunks to DB (Indexing Phase)
+        await asset_model.update_asset_status(asset_id, AssetStatusEnum.IDNEXING.value)
+
+        # nlp_controller instance
+
+        nlp_controller = NLPController(generation_client= request.app.generation_client,
+                                    embedding_client=request.app.embedding_client,
+                                    vectordb_client= request.app.vectordb_client,
+                                    template_parser=request.app.template_parser)
+        
+        # index chunks into vector db
+        _ = nlp_controller.index_into_vectordb(Project=project, chunks= file_chunks_records)
+
+        # STEP 3: Update status to SUCCESS / COMPLETED
+        await asset_model.update_asset_status(asset_id, AssetStatusEnum.SUCCESS.value)
+
+    except Exception as e:
+        traceback.print_exc()
+        print(f"{e}")
+        # Mark as error so the UI can show the error
+        await asset_model.update_asset_status(asset_id, AssetStatusEnum.ERROR.value)
+
 
 # upload endpoint
 @data_router.post("/upload/{project_id}")
@@ -30,6 +116,9 @@ async def upload_data(
     request: Request,  # the reqeust object has the app and its data
     project_id: str,
     file: UploadFile,
+    process_request: ProcessRequest,
+    upload_request: UploadRequest,
+    background_tasks: BackgroundTasks,
     app_settings: Settings = Depends(get_settings),
 ):
 
@@ -76,35 +165,61 @@ async def upload_data(
             content={"signal": ResponseSignal.FILE_UPLOAD_FAILED.value},
         )
 
+    
+
+    # find asset type
+    process_cotroller = ProcessController(project_id=project_id)
+    asset_ext = process_cotroller.get_file_extension(file_id=file_id)
+    asset_type = ""
+
+    if asset_ext == ProcessingEnum.PDF.value:
+        asset_type = AssetTypeEnum.PDF.value
+    
+    if asset_ext == ProcessingEnum.TEXT.value:
+        asset_type = AssetTypeEnum.TEXT.value
+    
+    # create asset resource
     asset_model = await AssetModel.create_instance(db_client=request.app.db_client)
 
     asset_resource = Asset(
         asset_project_id=project.id,
-        asset_type=AssetTypeEnum.FILE.value,
-        asset_name=file_id,
+        asset_type= asset_type,
+        asset_name=file.filename,
         asset_size=os.path.getsize(file_path),
+        asset_pushed_at = datetime.now(timezone.utc),
+        asset_status="",
+        asset_uploader_admin_id = ObjectId(upload_request.uploader_admin_id),
+        asset_uploader_admin_name = upload_request.uploader_admin_name
     )
 
     asset_record = await asset_model.create_asset(asset=asset_resource)
+
+    # 2. Trigger the background pipeline
+    background_tasks.add_task(
+        process_and_index_pipeline,
+        request= request,
+        project_id=str(project.project_id),
+        asset_id=str(asset_record.id),
+        file_id=file_id,
+        db_client=request.app.db_client,
+        process_request= process_request,
+    )
 
     # Upload completed successfully
     return JSONResponse(
         content={
             "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
             "file_id": str(asset_record.id),
+            "file_size": asset_record.asset_size,
+            "file_type": asset_record.asset_type,
+            "file_uploading_time": str(asset_record.asset_pushed_at)
         }
     )
 
-
-# process endpoint
-@data_router.post("/process/{project_id}")
-async def process_endpoint(
-    request: Request,  # the reqeust object has the app and its data
-    project_id: str,
-    process_request: ProcessRequest,
-):
-
-    # get projects collection or create it
+@data_router.get("/list/{project_id}")
+async def list_uploaded_files(request: Request, project_id: str):
+    
+     # get projects collection or create it
     db_client = request.app.db_client  # get the db_client
 
     project_model = await ProjectModel.create_instance(
@@ -113,101 +228,36 @@ async def process_endpoint(
 
     project = await project_model.get_project_or_create_one(project_id=project_id)
 
-    chunk_size = process_request.chunk_size
-    chunk_overlap = process_request.chunk_overlap
-    do_reset = process_request.do_reset
-
-    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
-    project = await project_model.get_project_or_create_one(project_id=project_id)
-
-    asset_model = await AssetModel.create_instance(db_client=request.app.db_client)
-
-    project_files_ids = {}
-    if process_request.file_id:
-        asset_record = await asset_model.get_asset_record(
-            asset_project_id=project.id, asset_name=process_request.file_id
-        )
-
-        if asset_record is None:
-            return JSONResponse(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                content={
-                    "signal": ResponseSignal.FILE_ID_ERROR.value,
-                },
-            )
-
-        project_files_ids = {asset_record.id: asset_record.asset_name}
-    else:
-
-        project_files = await asset_model.get_all_project_assets(
-            asset_project_id=project.id,
-            asset_type=AssetTypeEnum.FILE.value,
-        )
-
-        project_files_ids = {record.id: record.asset_name for record in project_files}
-
-    if len(project_files_ids) == 0:
+    # project not found
+    if not project:
         return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            content={
-                "signal": ResponseSignal.NO_FILES_ERROR.value,
-            },
-        )
-
-    # create Process Controller object
-    process_cotroller = ProcessController(project_id=project_id)
-
-    no_records = 0
-    no_files = 0
-
-    chunk_model = await ChunkModel.create_instance(db_client=request.app.db_client)
-
-    if do_reset == True:
-        _ = await chunk_model.delete_chunks_by_project_id(project_id=project.id)
-
-    for asset_id, file_id in project_files_ids.items():
-        # get file content
-        file_content = process_cotroller.get_file_content(file_id)
-
-        if file_content is None:
-            logger.error(f"Error while processing file: {file_id}")
-            continue
-
-        # get file chunks
-        file_chunks = process_cotroller.process_file_content(
-            file_id=file_id,
-            file_content=file_content,
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap,
-        )
-
-        # if an  error occurs return 400 status code
-        if file_chunks is None or len(file_chunks) == 0:
-            return JSONResponse(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                content={"signal": ResponseSignal.PROCESSING_FAILED.value},
+                content={"signal": ResponseSignal.PROJECT_NOT_FOUND.value},
             )
+    
+    # chat model instance
+    asset_model = await AssetModel.create_instance(db_client)
 
-        file_chunks_records = [
-            DataChunk(
-                chunk_text=chunk.page_content,
-                chunk_metadata=chunk.metadata,
-                chunk_order=i + 1,
-                chunk_project_id=project.id,
-                chunk_asset_id=asset_id,
-            )
-            for i, chunk in enumerate(file_chunks)
-        ]
+    # get all chats
+    all_files = await asset_model.get_all_project_assets(asset_project_id = project.id)
+    
+    all_files = [file.model_dump() for file in all_files]
 
-        chunk_model = await ChunkModel.create_instance(db_client=request.app.db_client)
+    for file in all_files:
+        file["id"] = str(file["id"])
+        file["asset_pushed_at"] = str(file["asset_pushed_at"])
+        file["asset_uploader_admin_id"] = str(file["asset_uploader_admin_id"])
+        file["asset_project_id"] = str(file["asset_project_id"])
 
-        no_records += await chunk_model.add_many_chunks(data_chunks=file_chunks_records)
-        no_files += 1
-
+    # return response
+    if not all_files:
+        return JSONResponse(
+            status_code= status.HTTP_400_BAD_REQUEST,
+            content={"signal": ResponseSignal.LIST_CHATS_ERROR.value}
+        )
+    
     return JSONResponse(
-        content={
-            "signal": ResponseSignal.PROCESSING_SUCCESS.value,
-            "inserted_chunks": no_records,
-            "processed_files": no_files,
-        }
+        status_code= status.HTTP_200_OK,
+        content={"signal": ResponseSignal.LIST_CHATS_SUCCESS.value,
+                 "all_files": all_files}
     )
--- src/routes/schemes/data.py
+++ src/routes/schemes/data.py
@@ -6,4 +6,7 @@ class ProcessRequest(BaseModel):
     chunk_size: Optional[int] = 500
     chunk_overlap: Optional[int] = 50
     do_reset: Optional[bool] = False
-    
+
+class UploadRequest(BaseModel):
+    uploader_admin_id: Optional[str] = "123456789123456789aaaaaa"
+    uploader_admin_name: Optional[str] = "Abanoub Wagih"
